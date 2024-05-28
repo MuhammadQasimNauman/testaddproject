257bc17821375ad67cf2c2f26f4f684904bb9201
qasimnauman <m.qasimnauman@gmail.com>
2024-05-28 19:37:31 +0500

    addition of more files

---
+++ CWH - Web Tuts/Calculator with js/script.js
@@ -0,0 +1,8 @@
+const calculator = document.querySelector('.calculator');
+const key = calculator.querySelector('.calculator__keys');
+
+key.addEventListener('click', e => {
+    if (e.target.matches('button')) {
+
+    }
+});
